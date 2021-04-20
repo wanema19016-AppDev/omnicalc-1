4084b2316debc470c9d3880ecf77e3a107720c0b
wanema19016 <willane0@chicagobooth.edu>
2021-04-20 19:52:03 +0000

    square fully working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
 end
